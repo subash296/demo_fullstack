89d811958b6dafeea5414e724fb4fb8445c81cb2
subash296 <113686640+subash296@users.noreply.github.com>
2024-10-22 08:02:40 +0530

    Update product.store.js

--- frontend/src/store/product.store.js
+++ frontend/src/store/product.store.js
@@ -8,18 +8,18 @@ export  const useProductStore= create((set)=>({
         if(!newProduct.name || !newProduct.price || !newProduct.image){
             return ({message:"please fill all the input fields",success:false})
         }
-        const res=await axios.post('http://localhost:5000/api/products',newProduct) 
+        const res=await axios.post('/api/products',newProduct) 
         console.log(`created product : ${res.data.data}`);
         
         set((state) => ({ products: [...state.products, res.data.data] }));
         return { success: true, message: "Product created successfully" };
     },
     fetchProducts:async()=>{
-        const res=await axios.get('http://localhost:5000/api/products')
+        const res=await axios.get('/api/products')
         set({products:res.data.data}) 
     },
     updateProduct:async(updatedProduct,id)=>{
-        const res= await axios.put(`http://localhost:5000/api/products/${id}`,updatedProduct)
+        const res= await axios.put(`/api/products/${id}`,updatedProduct)
        
         if (!res.data.success) return { success: false, message: data.message };
         set((state)=>({
@@ -31,7 +31,7 @@ export  const useProductStore= create((set)=>({
 
     },
     deleteProduct: async (id)=>{
-        const res= await axios.delete(`http://localhost:5000/api/products/${id}`)
+        const res= await axios.delete(`/api/products/${id}`)
         if (!res.data.success) return { success: false, message: data.message };
         set((state)=>({products:state.products.filter((product)=>product._id!==id)}))
 		return { success: true, message: res.data.message };
